0d73d3dd822281ea63457e83ee91cf5563faa3f1
RamzanKi <91493572+RamzanKi@users.noreply.github.com>
2023-04-08 21:04:18 +0300

    c

--- src/main/java/com/example/javadevtesttask/repository/PurchaseRepository.java
+++ src/main/java/com/example/javadevtesttask/repository/PurchaseRepository.java
@@ -3,12 +3,7 @@ package com.example.javadevtesttask.repository;
 import com.example.javadevtesttask.entity.Purchase;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
-import java.time.LocalDate;
-import java.util.List;
-import java.util.Map;
 
 @Repository
 public interface PurchaseRepository extends JpaRepository<Purchase, Long> {
@@ -22,12 +17,12 @@ public interface PurchaseRepository extends JpaRepository<Purchase, Long> {
             "LIMIT 1", nativeQuery = true)
     String findMostPurchasedProductName();
 
-    @Query(value = "SELECT purchase.name\n" +
-            "FROM purchase\n" +
-            "         JOIN user_purchase ON purchase.id = user_purchase.purchase_id\n" +
-            "WHERE user_purchase.user_age = 18\n" +
-            "GROUP BY purchase.name\n" +
-            "ORDER BY SUM(user_purchase.count) DESC\n" +
+    @Query(value = "SELECT p.name " +
+            "FROM purchase p " +
+            "JOIN user_purchase up ON p.id = up.purchase_id " +
+            "WHERE up.user_age = 18 " +
+            "GROUP BY p.name " +
+            "ORDER BY SUM(up.count) DESC " +
             "LIMIT 1;", nativeQuery = true)
     String findPopularIn18();
 }
--- src/main/java/com/example/javadevtesttask/repository/UserPurchaseRepository.java
+++ src/main/java/com/example/javadevtesttask/repository/UserPurchaseRepository.java
@@ -1,6 +1,5 @@
 package com.example.javadevtesttask.repository;
 
-import com.example.javadevtesttask.entity.Purchase;
 import com.example.javadevtesttask.entity.UserPurchase;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -15,7 +14,7 @@ public interface UserPurchaseRepository extends JpaRepository<UserPurchase, Long
 
     @Query(value = "SELECT user_name, user_lastname " +
             "FROM user_purchase " +
-            "WHERE purchase_date >= DATE_TRUNC('month', NOW() - INTERVAL '6 months') " +
+            "WHERE purchase_date >= now() - interval '6 month' " +
             "GROUP BY user_name, user_lastname " +
             "ORDER BY SUM(count) DESC " +
             "LIMIT 1; ", nativeQuery = true)
